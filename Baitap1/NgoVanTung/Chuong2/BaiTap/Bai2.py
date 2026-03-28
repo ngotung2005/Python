9489867036328aa0fc724fc7304e34c7294e131b
ngotung2005 <ngotung2005a@gmail.com>
2026-03-28 11:30:39 +0700

    Delete Baitap1/NgoVanTung/Chuong2/BaiTap directory

--- Baitap1/NgoVanTung/Chuong2/BaiTap/Bai2.py
+++
@@ -1,8 +0,0 @@
-_a = int(input("Nhap canh a:"))
-_b = int(input("Nhap canh b:"))
-_c = int(input("Nhap canh c:"))
-if _a + _b > _c and _a + _c > _b and _b + _c > _a:
-    print("Do dai 3 canh tam giac ")
-else:
-    print("Day kp do dai ba canh tam giac")
-
